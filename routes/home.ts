8c86d26b6c6cc6a65747841d0019f09ed348779a
xmNuc <xmnuc@o2.pl>
2022-02-05 03:55:35 +0100

    1.82 commit

--- routes/home.ts
+++ routes/home.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from 'express';
 import { CookieMakerApp } from '..';
 import { rest } from '../decorators/rest.decorators';
 import { MyRouter } from '../types/my-router';
+import { RestDecoratorInfo } from '../types/rest-decorator';
 
 export class HomeRouter implements MyRouter {
   public readonly urlPrefix = '/';
@@ -12,10 +13,23 @@ export class HomeRouter implements MyRouter {
   }
 
   private setUpRoutes(): void {
-    // Reflect.get(this, '_restApiCalls');
+    const ar: RestDecoratorInfo[] = Reflect.get(this, '_restApiCalls') ?? [];
     console.log(Reflect.get(this, '_restApiCalls'));
+
+    for (const apiOp of ar) {
+      this.router[apiOp.httpMethod](
+        apiOp.path,
+        (this as any)[apiOp.propertyName]
+      );
+    }
+
     // this.router.get('/', this.home);
   }
+
+  @rest('get', '/xx')
+  private xx = (req: Request, res: Response): void => {
+    res.send('XX works');
+  };
 
   @rest('get', '/')
   private home = (req: Request, res: Response): void => {
